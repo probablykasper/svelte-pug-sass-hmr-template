8b6ff0ed774ce28a953c3e9dddb210405dcc4c20
Kasper <kasperkh.kh@gmail.com>
2020-12-19 21:42:38 +0100

    Replace css with sass

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,6 @@
 <script>
   import { onMount } from 'svelte'
 
-  export let name
-
   let counter = 0 // @hmr:keep
 
   onMount(() => {
@@ -15,31 +13,51 @@
   })
 </script>
 
-<template lang='pug'>
-main
-  h1 Hello {name}!
-  p Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.
-  p This page has been open for {counter}s.
-</template>
+<style lang='sass'>
+  :global(html)
+    position: relative
+    width: 100%
+    height: 100%
 
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
+  :global(body)
+    position: relative
+    width: 100%
+    height: 100%
+    color: #333
+    margin: 0
+    padding: 8px
+    box-sizing: border-box
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif
+  
+  :global()
+    a
+      color: rgb(0,100,200)
+      text-decoration: none
+    a:hover
+      text-decoration: underline
+    a:visited
+      color: rgb(0,80,160)
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
+  main
+    text-align: center
+    padding: 1em
+    max-width: 240px
+    margin: 0 auto
 
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
+  h1
+    color: #ff3e00
+    text-transform: uppercase
+    font-size: 4em
+    font-weight: 100
+
+  @media (min-width: 640px)
+    main
+      max-width: none
 </style>
+
+<template lang='pug'>
+  main
+    h1 Hello world!
+    p Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.
+    p This page has been open for {counter}s.
+</template>
